d86acbf726b3182d42489ac03b33e1c03828e889
jc-higuera <Estudiante@ISIS2603-031.sis.virtual.uniandes.edu.co>
2019-08-22 08:04:11 -0500

    Se agregó ObservadoresPersistence Test

--- s2_hackatones-back/src/test/java/co/edu/uniandes/csw/hackatones/test/persistence/ObservadoresPersistenceTest.java
+++ s2_hackatones-back/src/test/java/co/edu/uniandes/csw/hackatones/test/persistence/ObservadoresPersistenceTest.java
@@ -5,10 +5,53 @@
  */
 package co.edu.uniandes.csw.hackatones.test.persistence;
 
+import co.edu.uniandes.csw.hackatones.entities.ObservadoresEntity;
+import co.edu.uniandes.csw.hackatones.persistence.ObservadoresPersistence;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Assert;
+import org.junit.Test;
+import uk.co.jemos.podam.api.PodamFactory;
+import uk.co.jemos.podam.api.PodamFactoryImpl;
+
 /**
  *
  * @author jc.higuera
  */
-public class ObservadoresPersistenceTest {
+public class ObservadoresPersistenceTest 
+{
+    @Inject
+    private ObservadoresPersistence op;
+    
+    @PersistenceContext
+    private EntityManager em;
+    
+    @Deployment
+    public static JavaArchive createDeployment()
+    {
+        return ShrinkWrap.create(JavaArchive.class)
+                .addClass(ObservadoresEntity.class)
+                .addClass(ObservadoresPersistence.class)
+                .addAsManifestResource("META-INF/persistence.xml", "persistence.xml")
+                .addAsManifestResource("META-INF/beans.xml", "beans.xml");
+    }
+    
+    @Test
+    public void createObservadoresTest()
+    {
+        PodamFactory factory = new PodamFactoryImpl();
+        ObservadoresEntity observadores = factory.manufacturePojo(ObservadoresEntity.class);
+        
+        ObservadoresEntity oe = op.create(observadores);
+        
+        Assert.assertNotNull(oe);
+        
+        ObservadoresEntity entity = em.find(ObservadoresEntity.class, oe.getId());
+        Assert.assertEquals(observadores.getNombre(), entity.getNombre());
+    }
     
 }
