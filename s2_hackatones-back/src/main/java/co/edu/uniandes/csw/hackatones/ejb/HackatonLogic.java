a2654e49f6e7b7f379516e0872a4500f5138dd66
jc-higuera <jc.higuera@uniandes.edu.co>
2019-09-09 21:04:37 -0500

    Se terminó la clase HackatonLogic y su test

--- s2_hackatones-back/src/main/java/co/edu/uniandes/csw/hackatones/ejb/HackatonLogic.java
+++ s2_hackatones-back/src/main/java/co/edu/uniandes/csw/hackatones/ejb/HackatonLogic.java
@@ -8,6 +8,9 @@ package co.edu.uniandes.csw.hackatones.ejb;
 import co.edu.uniandes.csw.hackatones.entities.HackatonEntity;
 import co.edu.uniandes.csw.hackatones.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.hackatones.persistence.HackatonPersistence;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 
@@ -18,15 +21,58 @@ import javax.inject.Inject;
 @Stateless
 public class HackatonLogic {
     
+    private static final Logger LOGGER = Logger.getLogger(HackatonLogic.class.getName());
+    
     @Inject
     private HackatonPersistence persistence;
     
-    public HackatonEntity createHackaton(HackatonEntity hackaton)throws BusinessLogicException{
+    public HackatonEntity createHackaton(HackatonEntity hackaton)throws BusinessLogicException
+    {
         if(hackaton.getNombre()==null){
             throw new BusinessLogicException("El nombre de la hackaton está vacío");
         }
+        LOGGER.log(Level.INFO, "Inicia el proceso de creacion de la hackaton");
         hackaton= persistence.create(hackaton);
+        LOGGER.log(Level.INFO, "Termina el proceso de creacion de la hackaton");
         return hackaton;
     }
     
+    
+    public List<HackatonEntity> getHackatones()
+    {
+        LOGGER.log(Level.INFO, "Inicia el proceso de consultar todas las hackatones");
+        List<HackatonEntity> hackatones = persistence.findAll();
+        LOGGER.log(Level.INFO, "Termina el proceso de consultar todas las hackatones");
+        return hackatones;
+    }
+    
+    public HackatonEntity getHackaton(Long hackatonId)
+    {
+        LOGGER.log(Level.INFO, "Inicia el proceso de consultar la hackaton con id={0}", hackatonId);
+        HackatonEntity hackaton = persistence.find(hackatonId);
+        if(hackaton == null){
+            LOGGER.log(Level.SEVERE, "La hackaton con el id = {0} no existe", hackatonId);
+        }
+        LOGGER.log(Level.INFO, "Termina el proceso de consultar la hackaton con id={0}", hackatonId);
+        return hackaton;
+    }
+    
+    
+    public HackatonEntity updateHackaton(Long hackatonId, HackatonEntity hackatonEntity)
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de actualizar hackaton con id = {0}", hackatonId);
+        HackatonEntity newEntity = persistence.update(hackatonEntity);
+        LOGGER.log(Level.INFO, "Termina el proceso de actualizar la hackaton con id={0}", hackatonEntity.getId());
+        return newEntity;
+    }
+    
+    
+    public void deleteHackaton(Long hackatonId)
+    {
+        LOGGER.log(Level.INFO, "Inicia proceso de borrar hackaton con id = {0}", hackatonId);
+        HackatonEntity hackatonEntity = persistence.find(hackatonId);
+        persistence.delete(hackatonId);
+        LOGGER.log(Level.INFO, "Termina proceso de borrar hackaton con id = {0}", hackatonId);
+        
+    }
 }
--- s2_hackatones-back/src/test/java/co/edu/uniandes/csw/hackatones/test/logic/HackatonLogicTest.java
+++ s2_hackatones-back/src/test/java/co/edu/uniandes/csw/hackatones/test/logic/HackatonLogicTest.java
@@ -9,14 +9,18 @@ import co.edu.uniandes.csw.hackatones.ejb.HackatonLogic;
 import co.edu.uniandes.csw.hackatones.entities.HackatonEntity;
 import co.edu.uniandes.csw.hackatones.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.hackatones.persistence.HackatonPersistence;
+import java.util.ArrayList;
+import java.util.List;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import junit.framework.Assert;
+import javax.transaction.UserTransaction;
+import org.junit.Assert;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import uk.co.jemos.podam.api.PodamFactory;
@@ -37,6 +41,11 @@ public class HackatonLogicTest {
     @PersistenceContext()
     protected EntityManager em;
     
+    @Inject
+    private UserTransaction utx;
+    
+    private List<HackatonEntity> data = new ArrayList<HackatonEntity>();
+    
     @Deployment
     public static JavaArchive createDeployment()
     {
@@ -48,6 +57,40 @@ public class HackatonLogicTest {
                 .addAsManifestResource("META-INF/beans.xml", "beans.xml");
     }
     
+    
+    @Before
+    public void configTest() {
+        try {
+            utx.begin();
+            clearData();
+            insertData();
+            utx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                utx.rollback();
+            } catch (Exception e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+    
+    
+    private void clearData()
+    {
+        em.createQuery("delete from HackatonEntity").executeUpdate();
+    }
+    
+    private void insertData()
+    {
+        for(int i = 0; i<3; i++)
+        {
+            HackatonEntity entity = factory.manufacturePojo(HackatonEntity.class);
+            em.persist(entity);
+            data.add(entity);
+        }
+    }
+    
     @Test
     public void createHackatonTest() throws BusinessLogicException
     {
@@ -57,6 +100,12 @@ public class HackatonLogicTest {
         
         HackatonEntity entity = em.find(HackatonEntity.class, result.getId());
         Assert.assertEquals(entity.getNombre(), result.getNombre());
+        Assert.assertEquals(entity.getEspecificacion(), result.getEspecificacion());
+        Assert.assertEquals(entity.getFechaFin(), result.getFechaFin());
+        Assert.assertEquals(entity.getFechaInicio(), result.getFechaInicio());
+        Assert.assertEquals(entity.getNivel(), result.getNivel());
+        Assert.assertEquals(entity.getTema(), result.getTema());
+        Assert.assertEquals(entity.getFinalizada(), result.getFinalizada());
     }
     
     @Test(expected = BusinessLogicException.class)
@@ -67,4 +116,56 @@ public class HackatonLogicTest {
         HackatonEntity result = hackatonLogic.createHackaton(newEntity);
         
     }
+    
+    @Test
+    public void getHackatonesTest()
+    {
+        List<HackatonEntity> list = hackatonLogic.getHackatones();
+        Assert.assertEquals(data.size(), list.size());
+        for(HackatonEntity entity: list)
+        {
+            boolean found = false;
+            for(HackatonEntity storedEntity: data)
+            {
+                if (entity.getId().equals(storedEntity.getId())) 
+                {
+                    found = true;
+                }
+            }
+            Assert.assertTrue(found);
+        }
+    }
+    
+    @Test
+    public void getHackatonTest()
+    {
+        HackatonEntity entity = data.get(0);
+        HackatonEntity resultEntity = hackatonLogic.getHackaton(entity.getId());
+        Assert.assertNotNull(resultEntity);
+        Assert.assertEquals(entity.getId(), resultEntity.getId());
+        Assert.assertEquals(entity.getNombre(), resultEntity.getNombre());
+    }
+    
+    @Test
+    public void updateHackatonTest()
+    {
+        HackatonEntity entity = data.get(0);
+        HackatonEntity pojoEntity = factory.manufacturePojo(HackatonEntity.class);
+        pojoEntity.setId(entity.getId());
+        hackatonLogic.updateHackaton(pojoEntity.getId(), pojoEntity);
+        HackatonEntity resp = em.find(HackatonEntity.class, entity.getId());
+        Assert.assertEquals(pojoEntity.getId(), resp.getId());
+        Assert.assertEquals(pojoEntity.getNombre(), resp.getNombre());
+    }
+    
+    @Test
+    public void deleteHackatonTest()
+    {
+        HackatonEntity entity = data.get(0);
+        hackatonLogic.deleteHackaton(entity.getId());
+        HackatonEntity deleted = em.find(HackatonEntity.class, entity.getId());
+        Assert.assertNull(deleted);
+    }
+    
+    
 }
