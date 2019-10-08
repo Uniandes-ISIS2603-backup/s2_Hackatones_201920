d3d601c6dd5c775efa96231475f904e2f6bf381f
a.pedraza <a.pedraza@uniandes.edu.co>
2019-10-08 18:18:10 -0500

    Se adelanto algo de calificacionIT y se agregaron cosas al .sql

--- s2_hackatones-api/sql/s2_hackatones.sql
+++ s2_hackatones-api/sql/s2_hackatones.sql
@@ -1,3 +1,8 @@
 delete from LugarEntity;
+delete from CalificacionEntity;
+delete from HackatonEntity;
 
 insert into LugarEntity(id,ciudad,direccion,nombre) values (987654321,'Bogotá','Carrera 90a # 8a -10', 'Centro empresarial ANDES');
+insert into HackatonEntity(id,especificacion,fechafin,fechainicio,finalizada,nivel,nombre,tema,lugar_id) values (123,'ninguna','4/7/2019','1/7/2019',1,3,'prueba','ninguno',987654321);
+insert into CalificacionEntity(id,calificacion,comentario,hackaton_id) values (0,4.9,'Todo bien',123);
+
