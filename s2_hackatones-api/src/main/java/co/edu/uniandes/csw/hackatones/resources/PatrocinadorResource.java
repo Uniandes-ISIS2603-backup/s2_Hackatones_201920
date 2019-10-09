b38b8ca849f28142380749c03656126393718ce5
s.estupinan@uniandes.edu.co <Estudiante@ISIS2603-032.sis.virtual.uniandes.edu.co>
2019-10-09 09:14:55 -0500

    Se terminaron los recursos de lenguaje, patrcinador y usuario.

--- s2_hackatones-api/src/main/java/co/edu/uniandes/csw/hackatones/resources/PatrocinadorResource.java
+++ s2_hackatones-api/src/main/java/co/edu/uniandes/csw/hackatones/resources/PatrocinadorResource.java
@@ -6,8 +6,11 @@
 package co.edu.uniandes.csw.hackatones.resources;
 
 import co.edu.uniandes.csw.hackatones.dtos.PatrocinadorDTO;
+import co.edu.uniandes.csw.hackatones.dtos.PatrocinadorDetailDTO;
 import co.edu.uniandes.csw.hackatones.ejb.PatrocinadorLogic;
+import co.edu.uniandes.csw.hackatones.entities.PatrocinadorEntity;
 import co.edu.uniandes.csw.hackatones.exceptions.BusinessLogicException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,6 +20,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -27,7 +31,7 @@ import javax.ws.rs.core.MediaType;
  *
  * @author s.estupinan
  */
-@Path("patrocinadores")
+@Path("/patrocinadores")
 @Consumes(MediaType.APPLICATION_JSON)
 @Produces(MediaType.APPLICATION_JSON)
 @RequestScoped
@@ -42,12 +46,69 @@ public class PatrocinadorResource {
     @POST
     public PatrocinadorDTO createPatrocinador(PatrocinadorDTO dto) throws BusinessLogicException {
         PatrocinadorDTO patrocinadorDTO = new PatrocinadorDTO(logic.createPatrocinador(dto.toEntity()));
-        return dto;
+        return patrocinadorDTO;
     }
     
-//    @GET
-//    public PatrocinadorDTO createReview(@PathParam("patrocinadorId") Long id, PatrocinadorDTO dto) throws BusinessLogicException {
-//        
+    @GET
+    public List<PatrocinadorDetailDTO> getUsuarios()
+    {
+        List<PatrocinadorDetailDTO> lista = listEntity2DTO(logic.getPatrocinadores());
+        return lista;
+    }
+    
+    
+    @GET
+    @Path("{patrocinadorId: \\d+}")
+    public PatrocinadorDetailDTO getPatrocinador(@PathParam("patrocinadorId") Long id) {
+        LOGGER.log(Level.INFO, "PatrocinadorResource getPatrocinador: input: {0}", id);
+        PatrocinadorEntity entity = logic.getPatrocinador(id);
+        if (entity == null) {
+            throw new WebApplicationException("El recurso /patrocinadores/" + id + " no existe.", 404);
+        }
+        PatrocinadorDetailDTO detailDTO = new PatrocinadorDetailDTO(entity);
+        LOGGER.log(Level.INFO, "PatrocinadorResource getPatrocinador: output: {0}", detailDTO);
+        return detailDTO;
+    }
+    
+    @PUT
+    @Path("{patrocinadorId: \\d+}")
+    public PatrocinadorDetailDTO updatePatrocinador(@PathParam("patrocinadorId") Long id, PatrocinadorDetailDTO patrocinador) {
+        LOGGER.log(Level.INFO, "PatrocinadorResource updatePatrocinador: input: patrocinadorId: {0} , usuario: {1}", new Object[]{id, patrocinador});
+        patrocinador.setId(id);
+        if (logic.getPatrocinador(id) == null) {
+            throw new WebApplicationException("El recurso /patrocinadores/" + id + " no existe.", 404);
+        }
+        PatrocinadorDetailDTO detailDTO = new PatrocinadorDetailDTO(logic.updatePatrocinador(id, patrocinador.toEntity()));
+        LOGGER.log(Level.INFO, "PatrocinadorResource updatePatrocinador: output: {0}", detailDTO);
+        return detailDTO;
+    }
+    
+    @DELETE
+    @Path("{patrocinadorId: \\d+}")
+    public void deletePatrocinador(@PathParam("patrocinadorId") Long id) throws BusinessLogicException {
+        LOGGER.log(Level.INFO, "PatrocinadorResource deletePatrocinador: input: {0}", id);
+        if (logic.getPatrocinador(id) == null) {
+            throw new WebApplicationException("El recurso /patrocinadores/" + id + " no existe.", 404);
+        }
+        logic.deletePatrocinador(id);
+        LOGGER.info("PatrocinadorResource deletePatrocinador: output: void");
+    }
+    
+//    @Path("{usuarioId: \\d+}/books")
+//    public Class<AuthorBooksResource> getAuthorBooksResource(@PathParam("usuarioId") Long id) {
+//        if (logic.getUsuario(id) == null) {
+//            throw new WebApplicationException("El recurso /usuarios/" + id + " no existe.", 404);
+//        }
+//        return AuthorBooksResource.class;
 //    }
+    
+
+    private List<PatrocinadorDetailDTO> listEntity2DTO(List<PatrocinadorEntity> entityList) {
+        List<PatrocinadorDetailDTO> list = new ArrayList<>();
+        for (PatrocinadorEntity entity : entityList) {
+            list.add(new PatrocinadorDetailDTO(entity));
+        }
+        return list;
+    }
     
 }
