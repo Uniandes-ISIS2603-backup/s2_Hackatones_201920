d78a95a9c60e1e6ed0ef28b393989bfd1fd2ed92
AlejandroPedrazaL <54148310+AlejandroPedrazaL@users.noreply.github.com>
2019-11-07 08:47:08 -0500

    Se solucionaron 2 issues

--- s2_hackatones-back/src/main/java/co/edu/uniandes/csw/hackatones/ejb/InteresParticipanteLogic.java
+++ s2_hackatones-back/src/main/java/co/edu/uniandes/csw/hackatones/ejb/InteresParticipanteLogic.java
@@ -11,12 +11,14 @@ import co.edu.uniandes.csw.hackatones.persistence.InteresPersistence;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.ejb.Stateless;
 import javax.inject.Inject;
 
 /**
  *
  * @author ja.torresl
  */
+@Stateless
 public class InteresParticipanteLogic {
     
     
